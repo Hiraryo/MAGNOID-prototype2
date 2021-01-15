165de1772e3cc5a1ca3f421c06a6006664f8ff26
Hiraryo <kalium.collecter@gmail.com>
2021-01-15 21:55:59 +0900

    Added motion switching process according to movement speed to HAL2_Controller.cs and create new LogDisplay.cs to display log on screen

--- Assets/Programmer/System/Game/HAL2_Controller.cs
+++ Assets/Programmer/System/Game/HAL2_Controller.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class HAL2_Controller : MonoBehaviour
 {
-    public float speed = 5f;
-    public float rotateSpeed = 120f;
+    public float Moving_speed = 5f;
+    public float Rotate_speed = 120f;
+    public float Jump_power = 400.0f;
+    private bool Grounded;
+    private Vector3 moveDirection = Vector3.zero;
+    private Animator animator;
+    private Rigidbody rb;
 
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
     void FixedUpdate(){
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
@@ -17,9 +26,29 @@ public class HAL2_Controller : MonoBehaviour
         velocity = transform.TransformDirection(velocity);
 
         // Character move
-        transform.localPosition += velocity * speed * Time.fixedDeltaTime;
+        transform.localPosition += velocity * Moving_speed * Time.fixedDeltaTime;
 
         // Character rotation
-        transform.Rotate(0, h * rotateSpeed * Time.fixedDeltaTime, 0);
+        transform.Rotate(0, h * Rotate_speed * Time.fixedDeltaTime, 0);
+
+        moveDirection = Moving_speed * v * gameObject.transform.forward;
+        if(Grounded == true){
+            if(moveDirection.magnitude > 0.1f){
+                animator.SetFloat("Speed",moveDirection.magnitude);
+            }else{
+                animator.SetFloat("Speed", 0f);
+            }
+            if(Input.GetKeyDown(KeyCode.Space)){
+                Grounded = false;
+                rb.AddForce(Vector3.up * Jump_power);
+            }
+        }
+        Debug.Log("moveDirection : " + moveDirection + ", Position : " + transform.position + ", Rotation : " + transform.rotation);
+    }
+
+    void OnTriggerEnter(Collider HAL2){
+        if(HAL2.gameObject.tag == "Ground"){
+            Grounded = true;
+        }
     }
 }
---
+++ Assets/Programmer/System/Game/LogDisplay.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+public class LogDisplay : MonoBehaviour
+{
+    // Maximun number of logs saved
+    [SerializeField] int m_MaxLogCount = 20;
+    // Area to display logs
+    [SerializeField] Rect m_Area = new Rect(50, 0, 1000, 400);
+    // Variable to put the log string
+    Queue<string> m_LogMessages = new Queue<string>();
+    // This is used to combine the strings in the log
+    StringBuilder m_StringBuilder = new StringBuilder();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Register the function to be called when outputting the log in Application.logMessageReceived
+        Application.logMessageReceived += LogReceived;
+    }
+
+    // LogReceived is called when the log is output
+    void LogReceived(string text, string stackTrace, LogType type)
+    {
+        // Add logs to Queue
+        m_LogMessages.Enqueue(text);
+        // If the number of logs exceeds the upper limit, delete the oldest one
+        while(m_LogMessages.Count > m_MaxLogCount)
+        {
+            m_LogMessages.Dequeue();
+        }
+    }
+
+    void OnGUI()
+    {
+        // Reset the contents of StringBuilder
+        m_StringBuilder.Length = 0;
+
+        // Combine log strings
+        foreach (string s in m_LogMessages)
+        {
+            m_StringBuilder.Append(s).Append(System.Environment.NewLine);
+        }
+
+        // Display on screen
+        GUI.Label(m_Area, m_StringBuilder.ToString());
+    }
+}
