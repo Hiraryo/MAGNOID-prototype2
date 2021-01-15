91f9ad668bc10e68a81a56b1ce717075f41ce33c
Hiraryo <kalium.collecter@gmail.com>
2021-01-15 18:43:56 +0900

    Update character controller scripts

---
+++ Assets/Programmer/System/Game/HAL2_Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HAL2_Controller : MonoBehaviour
+{
+    public float speed = 5f;
+    public float rotateSpeed = 120f;
+
+    void FixedUpdate(){
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 velocity = new Vector3(0,0,v);
+
+        // Convert character orientation to orientation in local space.
+        velocity = transform.TransformDirection(velocity);
+
+        // Character move
+        transform.localPosition += velocity * speed * Time.fixedDeltaTime;
+
+        // Character rotation
+        transform.Rotate(0, h * rotateSpeed * Time.fixedDeltaTime, 0);
+    }
+}
--- Assets/Programmer/System/Game/HAL2_Move2.cs
+++ Assets/Programmer/System/Game/HAL2_Move2.cs
@@ -2,64 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class HAL2_Move2 : MonoBehaviour
 {
-    private Vector3 moveDirection = Vector3.zero;
-    private float gravity = 20.0f;
-    //キャラクターコントローラー
+    public float speed = 3.0f;
+    public float rotateSpeed = 3.0f;
+
     private CharacterController controller;
-    //アニメーター
-    private Animator animator;
-    private Vector3 targetDirection;
-    
-    private Vector3 currentRotateForward = Vector3.zero;
-    private float rotate_speed = 20.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    void Start(){
+        // Get the component
         controller = GetComponent<CharacterController>();
-        animator = GetComponent<Animator>();
-        //animation.Play("idle");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Rotate();
+    void Update(){
+        // Rotation
+        transform.Rotate(0,Input.GetAxis("Horizontal") * rotateSpeed, 0);
+
+        // Character direction in local space
+        Vector3 forward = transform.transform.forward;
+
+        float curSpeed = speed * Input.GetAxis("Vertical");
+
+        // Move with SimpleMove function
+        controller.SimpleMove(forward * curSpeed);
     }
-
-    protected void Rotate(){
-        // Get current movement vector.
-        Vector3 moveVelocity = controller.velocity;
-        moveVelocity.y = 0;
-
-        // If the movement vector is other than the zero vector, set it as the rotation vector.
-        if(moveVelocity != Vector3.zero){
-            currentRotateForward = moveVelocity;
-        }
-
-        // Get angle and direction of rotation.
-        float value = Mathf.Min(1, rotate_speed * Time.deltaTime / Vector3.Angle(transform.forward, currentRotateForward));
-        Vector3 newForward = Vector3.Slerp(transform.forward, currentRotateForward, value);
-
-        if(newForward != Vector3.zero){
-            transform.rotation = Quaternion.LookRotation(newForward, transform.up);
-        }
-    }
-/*
-    void FixedUpdate(){
-        moveDirection = Vector3.zero;
-        moveDirection.y -= gravity;
-
-        targetDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        if(targetDirection.magnitude > 0.1f){
-            transform.rotation = Quaternion.LookRotation(targetDirection);
-            moveDirection += transform.forward*0.0001f;
-            //animator.CrossFade("walk");
-        }else{
-            GetComponent<Animation>().CrossFade("idle");
-        }
-        //controller.Move(moveDirection*Time.deltaTime);
-    }
-    */
 }
